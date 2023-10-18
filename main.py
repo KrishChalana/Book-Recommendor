308c9c441b82e29b891f2204ea30b5cdb9b3dc7e
TheRugbro <128020995+TheRugbro@users.noreply.github.com>
2023-10-18 16:30:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import streamlit as st
-from model import data
-from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
 import numpy as np
+from sentence_transformers import SentenceTransformer, util
 
+# Load your data and embeddings
+from model import data  # Make sure to import your 'data' and 'embeddings' variables
+embeddings = np.load('embeddings.npy')
 model = SentenceTransformer('bert-base-nli-mean-tokens')
 
+# Page title
+st.title("Book Search App")
 
-embeddings = np.load('embeddings.npy')
+# Create a search bar with a placeholder
+search_term = st.text_input("Search for a book", "Enter book title or keywords...")
 
-# Create a search bar
-search_term = st.text_input("Search for a book")
-search_term_em = model.encode(search_term)
-sim_all = []
-for i in range(len(embeddings)):
-    sim  = cos_sim(search_term_em,embeddings[i])
-    sim_all.append(sim)
+if search_term:
+    # Encode the search term
+    search_term_em = model.encode(search_term)
+    sim_all = [util.pytorch_cos_sim(search_term_em, embeddings[i]) for i in range(len(embeddings))]
 
+    # Sort and get the top 10 matches
+    top_10_indices = np.argsort(sim_all, axis=None)[-10:][::-1]
 
+    if len(top_10_indices) > 0:
+        st.subheader("Top 10 Matching Books")
 
-# Use enumerate to get (index, value) pairs
-indexed_list = list(enumerate(sim_all))
-
-# Sort the list based on the values (second element of each tuple)
-sorted_list = sorted(indexed_list, key=lambda x: x[1], reverse=True)
-
-# Get the indices of the top 10 maximum values
-top_10_indices = [index for index, _ in sorted_list[:10]]
-
-
-
-# Display book thumbnails and titles based on the search term
-for i in top_10_indices:
-        st.image(data.iloc[i]['thumbnail'], caption=data.iloc[i]['title'], use_column_width=True)
-
-# Optionally, show a message if no results match the search term
-# if not any(search_term.lower() in title.lower() for title in books):
-#     st.write("No books found.")
-
+        # Display book thumbnails and titles with a smaller image size
+        for i in top_10_indices:
+            st.image(data.iloc[i]['thumbnail'], caption=data.iloc[i]['title'], use_column_width=False, width=100)
+    else:
+        st.warning("No books found matching your search term.")
+else:
+    st.info("Enter a search term above to find books.")
